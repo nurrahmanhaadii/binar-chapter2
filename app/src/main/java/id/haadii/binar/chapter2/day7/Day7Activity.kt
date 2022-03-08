727e65791ceba3be236a0e14e9a9e4b9cb414228
hadi.umma <nurrahman.hadi@umma.id>
2022-03-08 21:24:03 +0700

    add activity day 7

---
+++ app/src/main/java/id/haadii/binar/chapter2/day7/Day7Activity.kt
@@ -0,0 +1,12 @@
+package id.haadii.binar.chapter2.day7
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.haadii.binar.chapter2.R
+
+class Day7Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day7)
+    }
+}

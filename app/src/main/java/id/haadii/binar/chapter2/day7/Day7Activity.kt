847ff58f73090e05b04eadcbcdc9d685c7a41a70
hadi.umma <nurrahman.hadi@umma.id>
2022-03-08 21:49:44 +0700

    add space

--- app/src/main/java/id/haadii/binar/chapter2/day7/Day7Activity.kt
+++ app/src/main/java/id/haadii/binar/chapter2/day7/Day7Activity.kt
@@ -8,6 +8,7 @@ class Day7Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day7)
+
     }
 
 }
